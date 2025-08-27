16756b2fe694e659a4e7b0ff0b3027b057ad1495
Wahhaj <wahhajk9@gmail.com>
2025-08-28 01:15:08 +0500

    Login Functionality authController Added, Commited

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,3 +37,46 @@ catch(err){
 }
 
 }
+
+// loging functionality
+
+export const loginUser=async(req,res)=>{
+    try{
+    console.log("User Controller");
+    const{email,password}=req.body;
+    const findUser=await User.findOne({email});
+    if(!findUser)
+    {
+        return res.status(500).json({
+            success:false,
+            message:"User Does Not Exist"
+        })
+    }
+    const comarePassword=await bcrypt.compare(password,findUser.password)
+    if(!comarePassword)
+    {
+        return res.status(500).json({
+            success:false,
+            message:"Wrong Credentials"
+        })
+    }
+    const token=await jwt.sign({id:findUser._id},"pkfkdfddf",{expiresIn:"7d"})
+    findUser.password=undefined;
+    return res.status(200).json({
+        success:true,
+        message:"User Logged In Successully",
+        findUser,
+        token
+    })}
+    catch(err)
+    {
+        console.log(err);
+        return res.status(500).json({
+            success:false,
+            message:"Error in Login API",
+            err
+        })
+        
+    }
+    
+}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,9 @@
-import { registerUser } from "../controllers/authController.js";
+import { loginUser, registerUser } from "../controllers/authController.js";
 import express from "express"
 
 const router=express.Router();
 
 
 router.post("/register",registerUser)
+router.post("/login",loginUser)
 export default router;
