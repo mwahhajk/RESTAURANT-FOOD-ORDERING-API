ee9f2531af1aeb8435c78dc2ca48bc368b1ea487
Wahhaj <wahhajk9@gmail.com>
2025-08-30 05:23:53 +0500

    createRestaurantController commited

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,5 +1,58 @@
+import Resturant from "../models/restaurantSchema.js";
+
 export const createRestaurantController=async(req,res)=>{
     console.log("create restaurant");
+    try {
+    const {
+      title,
+      imageUrl,
+      foods,
+      time,
+      pickup,
+      delivery,
+      isOpen,
+      logoUrl,
+      rating,
+      ratingCount,
+      code,
+      coords,
+    } = req.body;
+    // validation
+    if (!title || !coords) {
+      return res.status(500).send({
+        success: false,
+        message: "please provide title and address",
+      });
+    }
+    const newResturant = new Resturant({
+      title,
+      imageUrl,
+      foods,
+      time,
+      pickup,
+      delivery,
+      isOpen,
+      logoUrl,
+      rating,
+      ratingCount,
+      code,
+      coords,
+    });
+
+    await newResturant.save();
+
+    res.status(201).send({
+      success: true,
+      message: "New Resturant Created successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error In Create Resturant api",
+      error,
+    });
+  }
     
 }
 
